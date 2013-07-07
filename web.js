d7f696e56e11fa5092dc7f65551b4ab8dc8ff886
cesarsalgado <cesarsvs@gmail.com>
2013-07-07 20:30:55 +0000

    web.js send response with contents of index.html

--- web.js
+++ web.js
@@ -2,8 +2,10 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var indexBuff = fs.readFileSync('index.html');
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(indexBuff.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
